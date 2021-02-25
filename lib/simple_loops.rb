ebc9e9db29befe43f0eb1296fe48ad8c1a4b35fb
Paul H Graham Jr <pjhg14@gmail.com>
2021-02-25 13:33:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,34 @@
 # Write your methods here
+def loop_message_five_times(message)
+  count = 0
+
+  while count < 5 do
+    puts message
+  end
+    
+end
+
+def loop_message_n_times(message, times)
+  count = 0
+  
+  while count < times do
+    puts message
+  end
+end
+
+def output_array(array)
+  count = 0
+
+  while array[count] do
+    puts array[count]
+  end
+end
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+
+  while array[count] do
+    new_array.push(array[count])
+  end
+end
