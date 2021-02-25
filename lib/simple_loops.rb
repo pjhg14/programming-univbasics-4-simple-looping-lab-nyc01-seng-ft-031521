9ad7262ae42d94c4407529abfa057f61781050c3
Paul H Graham Jr <pjhg14@gmail.com>
2021-02-25 13:33:31 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,12 +5,12 @@ def loop_message_five_times(message)
   while count < 5 do
     puts message
   end
-    
+
 end
 
 def loop_message_n_times(message, times)
   count = 0
-  
+
   while count < times do
     puts message
   end
